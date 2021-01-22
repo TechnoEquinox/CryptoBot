96a771c70b74e36eae6fd7016f4eeafcde49776b
Recursive Fruit <26304831+TechnoEquinox@users.noreply.github.com>
2021-01-22 13:43:13 -0500

    v1.2.1
    - Added shell scripting for easier use in server automation
    - Changed output when script successfully connects to Discord
    - Added console logging when a user makes a query 
    - Attempted to fix a known issue with the rounding of some values. Issue is still present but has been mitigated

---
+++ cryptobot.sh
@@ -0,0 +1,9 @@
+red=`tput setaf 1`
+background=`tput setab 7`
+reset=`tput sgr 0`
+
+echo -e "${red}CryptoBot v1.2.1\nCreated by: TechnoEquinox\nPlease report bugs on Github: ${background}https://github.com/TechnoEquinox/CryptoBot${reset}"
+
+echo "${red}Launching Script..."
+cd ~/CryptoBot_dev
+python3.6 cryptobot.py
